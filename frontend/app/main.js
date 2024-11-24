fc65c36ff22f5387a2978eb56a06b3fa6403c1d4
vindex10 <vindex10@gmail.com>
2024-11-24 12:11:07 +0100

    use modules instead of script imports

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,8 +1,10 @@
+import { map as Lmap, tileLayer as LtileLayer, popup as Lpopup } from "leaflet";
+
 const BACKEND_URL = "http://127.0.0.1:8000";
 
 function init_map() {
-	const map = L.map("map").setView([45.120053, 29.509277], 6);
-	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	const map = Lmap("map").setView([45.120053, 29.509277], 6);
+	LtileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		maxZoom: 19,
 		attribution:
 			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -26,7 +28,7 @@ async function onMapClick(e, map) {
 	const msg = data["hmax"]
 		? `Max observed wave height at (${str_lat}, ${str_lon}) is ${data["hmax"].toFixed(2)}m`
 		: `We don't have information about this location: (${str_lat}, ${str_lon})`;
-	L.popup().setLatLng(e.latlng).setContent(msg).openOn(map);
+	Lpopup().setLatLng(e.latlng).setContent(msg).openOn(map);
 }
 
 (() => {
